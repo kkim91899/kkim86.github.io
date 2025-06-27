e8da0674a521ee911a95c02ce9f8122757ec9506
kkim91899 <116703528+kkim91899@users.noreply.github.com>
2025-06-27 23:24:15 +0900

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,5 @@
+var typed6 = new Typed('.typography', {
+   strings: ['','안녕하세요!<br class="no_out" />신입 UI/UX 디자이너 김백환입니다.<br class="no_out" /> 포트폴리오 사이트에 방문해주셔서 감사합니다.'],
+   typeSpeed: 50,
+   loop: false
+});
